5df457d6fb5f894a78e6d2537390cbc54bfb5f1e
marco-parillo <32338352+marco-parillo@users.noreply.github.com>
2019-10-16 15:37:09 -0400

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -9,4 +9,9 @@ for iter in vowels:
     print("char:", iter)
     list.append(iter)
 print (list)
-# list.to_csv()
+
+with open('employee_file.csv', mode='w') as employee_file:
+    employee_writer = csv.writer(employee_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+    employee_writer.writerow(['John Smith', 'Accounting', 'November'])
+    employee_writer.writerow(['Erica Meyers', 'IT', 'March'])
