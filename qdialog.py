793680c0e4fabe44bda9e5669c1fcb9aebb2f4ff
Marco Parillo <maparillo@gmail.com>
2021-01-30 20:11:01 -0500

    Add qdialog from Qt for Python tutorial

---
+++ qdialog.py
@@ -0,0 +1,32 @@
+import sys
+from PySide6.QtWidgets import (QLineEdit, QPushButton, QApplication,
+    QVBoxLayout, QDialog)
+
+class Form(QDialog):
+
+    def __init__(self, parent=None):
+        super(Form, self).__init__(parent)
+        # Create widgets
+        self.edit = QLineEdit("Enter Row Number")
+        self.button = QPushButton("Accept")
+        # Create layout and add widgets
+        layout = QVBoxLayout()
+        layout.addWidget(self.edit)
+        layout.addWidget(self.button)
+        # Set dialog layout
+        self.setLayout(layout)
+        # Add button signal to greetings slot
+        self.button.clicked.connect(self.getrecord)
+
+    # Greets the user
+    def getrecord(self):
+        print ("Record %s" % self.edit.text())
+
+if __name__ == '__main__':
+    # Create the Qt Application
+    app = QApplication(sys.argv)
+    # Create and show the form
+    form = Form()
+    form.show()
+    # Run the main Qt loop
+    sys.exit(app.exec_())
